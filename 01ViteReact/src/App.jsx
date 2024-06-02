ade1be78f8e29f81838917a19fa848da673cfa60
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-06-03 00:14:05 +0530

    Final commit

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,29 @@
+
+// function App() {
+  
+
+//   return (
+//    <h1> Hello I am Laxmi Narayana </h1>
+//   )
+// }
+
+// export default App
+
+
+
+import Demo from "./demo"
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Demo/>
+    <h1>This is for demo with react {username}</h1>
+    <p>test para</p>
+   </>
+  )
+}
+
+export default App
---
+++ 01ViteReact/src/demo.jsx
@@ -0,0 +1,7 @@
+function Demo(){
+    return(
+        <h3>chai is ready</h3>
+    )
+}
+
+export default Demo
