a90229e0c10cd70d1d12e0ad05c2120e31082c12
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-06-16 18:33:22 +0530

    How react works internally

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -11,14 +12,17 @@ function MyApp(){
   )
 }
 
-const ReactElement = {
-    type: 'a',
-    props: {
-        href: 'https://google.com',
-        target: '_blank'
-    },
-    children: 'Click me to visit google'
-}
+
+
+// Whatever you written in the MyApp(), end of the day it formatted to object like below 
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
 
 
 
@@ -26,34 +30,52 @@ const ReactElement = {
 
 // This is for App() function
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-   </React.StrictMode>,
+  //  </React.StrictMode>,
 )
 
 
+// This is also a object it works but ReactElement object does not work because RootElement object synstax is not right. The key value properties is not right 
 const anotherElement = (
   <a href= "https://google.com" target = '_blank'>Visit Google</a>
 )
 
 
+
+const reactElement = React.createElement(
+  'a',
+  { href: "https://google.com", target: '_blank' },
+  '[Click to Google]'
+)
+  
+  
+  
+  
 ReactDOM.createRoot(document.getElementById('root')).
   render(
-    MyApp()  // at the end of the day you can simply call the function like JavaScript 
+    <MyApp />    // It is more optimized way of writing 
+    // MyApp()  // at the end of the day you can simply call the function like JavaScript 
 )
   
 
-// it does not run because ReactElement writing style is not parsed 
+// // it does not run because ReactElement writing style is not parsed 
+// ReactDOM.createRoot(document.getElementById('root')).
+//   render(
+//     ReactElement
+     
+// )
+
+
 ReactDOM.createRoot(document.getElementById('root')).
   render(
-    ReactElement
+    // <ReactElement />
+    reactElement
 )
 
 
+// It works fine 
 ReactDOM.createRoot(document.getElementById('root')).
   render(
     anotherElement
 )
-
-
-// 2:10
