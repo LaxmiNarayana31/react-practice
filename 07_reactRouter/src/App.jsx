392b38c69d81c7ddc5dd62a8092edb5230a2a8f4
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-09-22 08:37:34 +0530

    react router

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+// Link -> https://reactrouter.com/en/main
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-yellow-400 text-black p-1 rounded-xl">React Router</h1>
+    </>
+  );
+}
+
+export default App;
